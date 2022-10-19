740a586bbe6cdf6e8d8ef6d16c133afaef5bc811
SleepD4rt <gastonapares@hotmail.com>
2022-10-18 22:31:15 -0300

    universal setting.py linea 59

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -56,6 +56,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        #Ruta aniversal para los templates
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
